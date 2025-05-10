39e51bac0665d559903f1f747d0622557bded70a
sagorsarker04 <ramita.rafi@gmail.com>
2025-05-10 09:44:20 +0600

    08-05-2025

---
+++ Task 3/task3.go
@@ -0,0 +1,17 @@
+package main
+
+type Book struct {
+	Title           string
+	ISBN            string
+	Price           int
+	AvailableCopies int
+}
+
+type Member struct {
+	Name          string
+	BorrowedBooks []int
+}
+
+func Addbook(B Book) {
+
+}
